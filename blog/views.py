86ac1733f205b1d63bf6c2c9d55b731f964f7c77
Uijeong-Noh <niceuj84@gmail.com>
2020-08-01 11:40:42 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    plist = Post.objects.all()
+    template = 'blog\post_list.html'
+    context = {'list': plist}
+    return render(request, template, context)
